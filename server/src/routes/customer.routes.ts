3972ace080680fd3ac1bcfb654268ff1b1e0f50f
DilanLopezN <dilanlopez009@gmail.com>
2023-04-05 12:55:54 -0300

    add labs page

--- server/src/routes/customer.routes.ts
+++ server/src/routes/customer.routes.ts
@@ -5,9 +5,8 @@ import { customerController } from '../controllers/customerController';
 
 export async function customersRoutes(app: FastifyInstance)
 {
+  app.post('/customers', customerController.createUser)
   app.get('/customers', customerController.showAllUsers)
   app.get('/customers/search', customerController.searchUser )
-  app.post('/customers', customerController.createUser)
   app.get("/customers/:id", customerController.findUserById)
-
 }
