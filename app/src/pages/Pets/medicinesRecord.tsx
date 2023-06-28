8603fe574e07800f2a994edb2456e06f277671d2
DilanLopezN <dilanlopez009@gmail.com>
2023-06-28 10:47:05 -0300

    works2806

--- app/src/pages/Pets/medicinesRecord.tsx
+++ app/src/pages/Pets/medicinesRecord.tsx
@@ -82,7 +82,7 @@ export function MedicineRecords() {
 
       <MedicineContainer>
         <Flex direction="column">
-        <Flex w="100%" h="30%"  direction="row" justify="space-evenly" border="2px" rounded={4} align="center" className="onemain">
+        <Flex w="100%" h="30%"  direction="row" justify="space-evenly" rounded={4} align="center" className="onemain">
           <Flex p="4" gap={4} w="100%" h="100%" direction="column">
             <Text height="38px" fontWeight="bold">
               Cliente
@@ -98,7 +98,7 @@ export function MedicineRecords() {
             <Text fontWeight="bold" w="300px" border="2px" rounded={8} height="38px" textAlign="center" bgColor="gray.100" >
              {pets.customerName}
             </Text>
-            <Text  fontWeight="bold" w="300px" border="2px"  height="68px" rounded={8} bgColor="gray.100" textAlign="center" >
+            <Text  fontWeight="bold" w="300px" border="2px"  height="78px" rounded={8} bgColor="gray.100" textAlign="center" >
               {`Nome: ${pets.name}, Raça: ${pets.race}, Peso: ${pets.weigth}Kg's,  Sexo ${pets.sexo}, Cor: ${pets.corPet} `}
             </Text>
 
@@ -107,26 +107,58 @@ export function MedicineRecords() {
             </Text>
           </Flex>
         </Flex>
-        <Flex  direction="column" w="100%" h="70%" bgColor="gray.100" border="2px" rounded={4}  className="secondmain">
+        <Flex  direction="column" w="100%" h="70%" bgColor="gray.100" rounded={4}  className="secondmain">
           <Flex w="100%" h="10%" justify="space-evenly" m="2" >
             <Button colorScheme="whatsapp" >Consultas</Button>
             <Button colorScheme="whatsapp" >Internações</Button>
             <Button colorScheme="whatsapp" >Outra Unidade</Button>
           </Flex>
-          <Flex h="90%"  w="100%" overflow="auto" >
-          <Flex w="100%" h="88px" direction="column" border="2px" m="2">
-            <Text border="2px" fontWeight="bold" w="100%" h="38px" bgColor="cyan.100" > 27/06/2023 - 17:30  Consulta ESP Dr Daniel Henrique</Text>
-            <Text border="2px" fontWeight="bold"  w="100%" h="38px" bgColor="cyan.100"><span className="textSpan">Peso Época:</span> 5,9 Kgs</Text>
-            <Text border="2px" fontWeight="bold"  w="100%" h="38px" bgColor="cyan.100">Observações: Não há observações registradas nessa consulta</Text>
+          <Flex h="100%"  w="100%" overflow="auto" >
+          <Flex w="100%" h="auto" direction="column" m="2">
+            <Text  fontWeight="bold" w="100%" h="38px" bgColor="cyan.100" > 27/06/2023 - 17:30  Consulta ESP Dr Daniel Henrique</Text>
+            <Text  fontWeight="bold"  w="100%" h="38px" bgColor="cyan.100"><span className="textSpan">Peso Época:</span> 5,9 Kgs</Text>
+            <Text fontWeight="bold"  w="100%" h="58px" bgColor="cyan.100">Observações: Não há observações registradas nessa consulta</Text>
           </Flex>
           </Flex>
         </Flex>
         </Flex>
      
-        <Flex className="one"></Flex>
-        <Flex className="two"></Flex>
-        <Flex className="three"></Flex>
-        <Flex className="four"></Flex>
+        <Flex width="600px" direction="column" className="one" >
+
+         <Flex height="50%" w="100%" textAlign="center">
+          <Flex w="100%" height="38px" bgColor="gray.100" align="center" justify="center">
+          <Text fontWeight="bold" >VACINAS</Text>
+          </Flex>
+            
+         </Flex >
+
+
+         <Flex height="50%" w="100%" textAlign="center">
+          <Flex w="100%" height="38px" bgColor="gray.100" align="center" justify="center">
+          <Text fontWeight="bold" >CIRURGIAS</Text>
+          </Flex>
+            
+         </Flex >
+
+     
+        </Flex>
+
+
+
+        <Flex ml="1" width="600px" direction="column"  className="two">
+          <Flex height="50%" w="100%" textAlign="center">
+          <Flex w="100%" height="38px" bgColor="gray.100" align="center" justify="center">
+          <Text fontWeight="bold" >EXAMES</Text>
+          </Flex>
+            
+         </Flex >
+         <Flex height="50%" w="100%" textAlign="center">
+          <Flex w="100%" height="38px" bgColor="gray.100" align="center" justify="center">
+          <Text fontWeight="bold" >INTERNAÇÕES</Text>
+          </Flex>
+            
+         </Flex >
+        </Flex>
 
       </MedicineContainer>
       </Flex>
