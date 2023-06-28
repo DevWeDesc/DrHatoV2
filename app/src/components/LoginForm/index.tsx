8ac19d22f81eba45e0cc678a1ac16580e51d7ee4
DesCTi <desenvolvimento@descti.com.br>
2023-06-28 10:49:07 -0300

    Works 28,06

--- app/src/components/LoginForm/index.tsx
+++ app/src/components/LoginForm/index.tsx
@@ -1,52 +1,68 @@
-import { useContext  } from "react"
-import { LoginFormContainer } from "./styles"
-import { useNavigate} from 'react-router-dom'
+import { useContext } from "react";
+import { LoginFormContainer } from "./styles";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import { toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { Button, ChakraProvider, Input, Flex } from "@chakra-ui/react";
 import { defaultTheme } from "../../styles/themes/default";
 import { DbContext } from "../../contexts/DbContext";
 import { LoadingSpinner } from "../Loading";
 
 export function LoginForm() {
-    const { setUserPassword, setUserLogin, validateLogin} = useContext(AuthContext)
-    const { dbLoaded } = useContext(DbContext)
+  const { setUserPassword, setUserLogin, validateLogin } =
+    useContext(AuthContext);
+  const { dbLoaded } = useContext(DbContext);
 
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    if (validateLogin() === true) {
+      navigate("/Home");
+    } else {
+      toast.info("✋ login ou senha incorretos!");
+    }
+  };
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <LoginFormContainer>
+        <h1>
+          {" "}
+          Bem vindo(a) <span className="waving-hand">&#9995;</span>{" "}
+        </h1>
+        <Flex className="submitLogin">
+          <label htmlFor="login">Seu Login</label>
+          <Input
+            border="4px"
+            placeholder="seu login"
+            type="text"
+            onChange={(ev) => setUserLogin(ev.target.value)}
+          />
 
-    const navigate = useNavigate()
-    const handleLogin = () =>  {
-      
-            if(validateLogin() === true) {
-                    navigate('/Home')
-            }else {
-                toast.info('✋ login ou senha incorretos!',);
-            }
-            }
-    return (
+          <label htmlFor="password">Sua senha</label>
+          <Input
+            placeholder="sua senha"
+            type="password"
+            name=""
+            id=""
+            onChange={(ev) => setUserPassword(ev.target.value)}
+          />
 
-        <ChakraProvider theme={defaultTheme}>
-        <LoginFormContainer>
-          
-            <h1> Bem vindo(a) <span className="waving-hand">&#9995;</span> </h1>
-           <Flex className="submitLogin">
-            <label htmlFor="login">Seu Login</label>
-            <Input border="4px"  placeholder="seu login" type="text" onChange={ev => setUserLogin(ev.target.value) } />
-
-            <label htmlFor="password">Sua senha</label>
-            <Input   placeholder="sua senha" type="password" name="" id="" onChange={ev => setUserPassword(ev.target.value)} />
-
-            {
-                dbLoaded === true ? ( <Button colorScheme="whatsapp" border="8px" onClick={() => handleLogin()}>Entrar</Button>) : (<>Carregando base de Dados<LoadingSpinner/></>)
-            }
-
-           
- 
-            </Flex> 
-
-        
-      
-        </LoginFormContainer>
-        </ChakraProvider>
-    )
+          {dbLoaded === true ? (
+            <Button
+              colorScheme="whatsapp"
+              border="8px"
+              onClick={() => handleLogin()}
+            >
+              Entrar
+            </Button>
+          ) : (
+            <>
+              Carregando base de Dados
+              <LoadingSpinner />
+            </>
+          )}
+        </Flex>
+      </LoginFormContainer>
+    </ChakraProvider>
+  );
 }
--- app/src/pages/Vets/workspace.tsx
+++ app/src/pages/Vets/workspace.tsx
@@ -1,33 +1,54 @@
-import { WorkSpaceContainer, WorkSpaceHeader, WorkSpaceContent, WorkSpaceFooter } from "./styles";
-import { Text, Button,ChakraProvider, Flex, Table, Thead, Tr, Td, Tbody, Textarea, HStack, VStack} from '@chakra-ui/react'
-import { AiFillMedicineBox, BiHome, MdPets, TbArrowBack, TbMedicalCrossFilled} from 'react-icons/all'
+import {
+  WorkSpaceContainer,
+  WorkSpaceHeader,
+  WorkSpaceContent,
+  WorkSpaceFooter,
+} from "./styles";
+import {
+  Text,
+  Button,
+  ChakraProvider,
+  Flex,
+  Table,
+  Thead,
+  Tr,
+  Td,
+  Tbody,
+  Textarea,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  AiFillMedicineBox,
+  BiHome,
+  MdPets,
+  TbArrowBack,
+  TbMedicalCrossFilled,
+} from "react-icons/all";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { api } from "../../lib/axios";
 import { LoadingSpinner } from "../../components/Loading";
 import { SetExamForm } from "../../components/workspaceVet/SetExamForm";
 import { GenericModal } from "../../components/Modal/GenericModal";
-
-
-
+import { Select } from "@chakra-ui/react";
 
 type ExamsProps = [
   {
     id: number | string;
     name: string;
     price: string;
-    doneExam: boolean
+    doneExam: boolean;
     requestedData: string;
   }
-]
-
+];
 
 type QueueProps = {
   moreInfos: string;
   queueOur: string;
-}
+};
 export interface PetProps {
-  id: number
+  id: number;
   name: string;
   customerName: string;
   balance: number;
@@ -43,198 +64,316 @@ export interface PetProps {
   status: string;
   bornDate: string;
   exams: ExamsProps;
-  queue: QueueProps
+  queue: QueueProps;
   recordId: string | number;
 }
 
-
 export function WorkSpaceVet() {
-
   const { id } = useParams<{ id: string }>();
-  const navigate = useNavigate()
-  const [pet, setPet] = useState({} as PetProps)
-  const [modalTwo, setModalTwoOpen] = useState(false)
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const navigate = useNavigate();
+  const [pet, setPet] = useState({} as PetProps);
+  const [modalTwo, setModalTwoOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   function openModal() {
-    setIsModalOpen(true)
+    setIsModalOpen(true);
   }
   function closeModal() {
-    setIsModalOpen(false)
+    setIsModalOpen(false);
   }
 
-function openModalTwo() {
-  setModalTwoOpen(true)
-}
-function closeModalTwo() {
-  setModalTwoOpen(false)
-}
-
+  function openModalTwo() {
+    setModalTwoOpen(true);
+  }
+  function closeModalTwo() {
+    setModalTwoOpen(false);
+  }
 
   useEffect(() => {
     async function getPetDetails() {
-      const response = await api.get(`/pets/${id}`)
-      setPet(response.data)
+      const response = await api.get(`/pets/${id}`);
+      setPet(response.data);
     }
-     getPetDetails()
+    getPetDetails();
+  }, []);
 
-  },[])
-
-
-  console.log("DETALHES PET", pet)
-''
+  console.log("DETALHES PET", pet);
+  ("");
 
   return (
     <ChakraProvider>
-       <WorkSpaceContainer>
-      <WorkSpaceHeader>
-        <Flex justify="space-between" align="center" width="100%" height="100%">
-          <Flex align="center" gap="2" >
-          <Text m="2" fontSize="2xl" fontWeight="bold">WorkSpace Veterinário</Text>
-          <Button colorScheme="teal" leftIcon={<BiHome size={24}/>}
-          onClick={() => navigate('/Home')}
-          >Home</Button>
-
-          <Button colorScheme="yellow" leftIcon={<TbArrowBack size={24}/>}
-          onClick={() => navigate('/Vets/Menu')}
-          >Voltar</Button>
-          </Flex>
-       
-        <Flex flexWrap="wrap" justify="space-between" gap="2" m="4" p="2">
-        <Button height={8} colorScheme="whatsapp">FORMULÁRIOS</Button>
-        <Button height={8} colorScheme="whatsapp">INSTRUÇÕES PROPRIETÁRIO</Button>
-        <Button height={8} colorScheme="whatsapp">AUTORIZAÇÕES</Button>
-        <Button height={8} colorScheme="whatsapp">PROTOCOLOS</Button>
-        <Button height={8} colorScheme="whatsapp" onClick={() => openModal()}>EXAMES</Button>
-        <Button height={8} colorScheme="whatsapp">PROCEDIMENTOS</Button>
-        <Button height={8} colorScheme="whatsapp">VACINAS</Button>
-        <Button height={8} leftIcon={<MdPets/>} colorScheme="messenger">PRONTUÁRIO DO PET</Button>
-    
-        </Flex>
-        
-        </Flex>
-       
-
-      </WorkSpaceHeader>
-      <WorkSpaceContent>
-      <div className="div1">
-        <Flex m="4" gap="2"  direction="row" >
-          <VStack>
-          <Text  fontWeight="bold"  >Cliente</Text>
-          <Text  fontWeight="bold"  >Gastos</Text>
-          <Text  fontWeight="bold"  >Animal</Text>
-          <Text  fontWeight="bold"  >Horário</Text>
-          <Text  fontWeight="bold"  >Internações</Text>
-          <Text  fontWeight="bold"  >Plano de Saúde</Text>
-          </VStack>
-        <VStack>
-          <Text border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100">{pet.customerName}</Text>
-          <Text
-          border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100"
-          >{new Intl.NumberFormat("pt-BR",{
-                currency: "BRL",
-                style: "currency"
-              }).format(pet.balance)}</Text>
-              <Text
-              border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100"
-              >
-              {`${pet.name}, ${pet.race}`}
-              </Text>
-
-              <Text
-              border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100"
-              >
-              {`${pet.sexo}, ${pet.weigth}`}
-              </Text>
-              <Text
-              border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100"
-              >{pet.queue?.queueOur}</Text>
-              <Text
-              border="1px" width="400px" rounded="4px"  fontWeight="bold" textAlign="center" bgColor="gray.100"
-              >NÃO POSSUI</Text>
-        </VStack>
-       
-        </Flex >
-        <Flex direction="column" m="4">
-            <Text fontSize="lg" m="2">Observações</Text>
-            <Textarea color="red.900" value={pet.queue?.moreInfos}></Textarea>
-          </Flex>
-      </div>
-        <Flex direction="column" className="div2">
-          <Flex backgroundColor="cyan.100" w="100%" h="48px" direction="row" align="center" justify="center">
-            <Text mr="1">Exames - </Text>
-            <Text mr="2" color="red">Vermelho: Por fazer  </Text>
-            /
-            <Text ml="2" color="green">Verde: pronto</Text>
-          </Flex >
-        
-          <Flex m="2" direction="column" gap="2" overflow="auto"
+      <WorkSpaceContainer>
+        <WorkSpaceHeader>
+          <Flex
+            justify="space-between"
+            align="center"
+            width="100%"
+            height="100%"
           >
-            {
-              pet.exams ? pet.exams.map((exam) => (
-                <Flex key={exam.id} w="100%" backgroundColor="gray.100" p="2" justify="space-between">
+            <Flex align="center" gap="2">
+              <Text m="2" fontSize="2xl" fontWeight="bold">
+                WorkSpace Veterinário
+              </Text>
+              <Button
+                colorScheme="teal"
+                leftIcon={<BiHome size={24} />}
+                onClick={() => navigate("/Home")}
+              >
+                Home
+              </Button>
 
-                <>{exam.doneExam === true ? (<Text color="green.400" fontWeight="bold">{exam.name}</Text>) : (<Text color="red.400" fontWeight="bold">{exam.name}</Text>)}</>
-                <Text>{exam.requestedData }</Text>
-                </Flex>
-              )) : (<Text>Sem exame Solicitado</Text>)
-            }
-       
-        
-       
-          </Flex>
-      
-        </Flex>
-      <div className="div3"> 
-      <HStack spacing={4} m="2">
-        <Button colorScheme="whatsapp">Diagnóstico</Button>
-        <Button colorScheme="whatsapp">Prescrição</Button>
-        <Button colorScheme="whatsapp">Sintomas</Button>
-        <Button colorScheme="whatsapp">Solicitar Exame</Button>
-        </HStack>
-        <Textarea border="1px" minHeight={220} m="2" maxWidth={600}></Textarea>
-      </div>
-      <div className="div4">
-
-      <Flex justify="space-between" gap="2" m="2">
-        <Button leftIcon={<MdPets/>} height={8} colorScheme="whatsapp">Outros Animais</Button>
-        <Button leftIcon={<TbMedicalCrossFilled/>} height={8} colorScheme="whatsapp">Vacinas</Button>
-        <Button leftIcon={<AiFillMedicineBox/>} height={8} colorScheme="whatsapp">Cirurgias</Button>
-        </Flex>
-
-
-        <Flex m="2" direction="column" gap="2">
-            <Flex w="100%" backgroundColor="gray.100" p="4" justify="space-between">
-           
+              <Button
+                colorScheme="yellow"
+                leftIcon={<TbArrowBack size={24} />}
+                onClick={() => navigate("/Vets/Menu")}
+              >
+                Voltar
+              </Button>
             </Flex>
-          
-          <Flex w="100%" backgroundColor="gray.100" p="4" justify="space-between">
-          
+
+            <Flex flexWrap="wrap" justify="space-between" gap="2" m="4" p="2">
+              <Button height={8} colorScheme="whatsapp">
+                FORMULÁRIOS
+              </Button>
+              <Button height={8} colorScheme="whatsapp">
+                INSTRUÇÕES PROPRIETÁRIO
+              </Button>
+              <Button height={8} colorScheme="whatsapp">
+                AUTORIZAÇÕES
+              </Button>
+              <Button height={8} colorScheme="whatsapp">
+                PROTOCOLOS
+              </Button>
+              <Button
+                height={8}
+                colorScheme="whatsapp"
+                onClick={() => openModal()}
+              >
+                EXAMES
+              </Button>
+              <Button height={8} colorScheme="whatsapp">
+                PROCEDIMENTOS
+              </Button>
+              <Button height={8} colorScheme="whatsapp">
+                VACINAS
+              </Button>
+              <Button height={8} leftIcon={<MdPets />} colorScheme="messenger">
+                PRONTUÁRIO DO PET
+              </Button>
+            </Flex>
           </Flex>
+        </WorkSpaceHeader>
+        <WorkSpaceContent>
+          <div className="div1">
+            <Flex m="4" gap="2" direction="row">
+              <VStack>
+                <Text fontWeight="bold">Cliente</Text>
+                <Text fontWeight="bold">Gastos</Text>
+                <Text fontWeight="bold">Animal</Text>
+                <Text fontWeight="bold">Horário</Text>
+                <Text fontWeight="bold">Internações</Text>
+                <Text fontWeight="bold">Plano de Saúde</Text>
+              </VStack>
+              <VStack>
+                <Text
+                  border="1px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                >
+                  {pet.customerName}
+                </Text>
+                <Text
+                  border="1px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                >
+                  {new Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    style: "currency",
+                  }).format(pet.balance)}
+                </Text>
+                <Text
+                  border="1px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                >
+                  {`${pet.name}, ${pet.race}`}
+                </Text>
 
-          <Flex w="100%" backgroundColor="gray.100" p="4" justify="space-between">
-          
+                <Text
+                  border="1px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                >
+                  {`${pet.sexo}, ${pet.weigth}`}
+                </Text>
+                <Text
+                  border="1px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                >
+                  {pet.queue?.queueOur}
+                </Text>
+                <Select
+                  border="1px"
+                  height="26px"
+                  width="400px"
+                  rounded="4px"
+                  fontWeight="bold"
+                  textAlign="center"
+                  bgColor="gray.100"
+                  placeholder="Selecione uma Opção"
+                >
+                  <option value="option1">Não tenho plano de saúde</option>
+                  <option value="option2">Pet Love</option>
+                </Select>
+              </VStack>
+            </Flex>
+            <Flex direction="column" m="4">
+              <Text fontSize="lg" m="2">
+                Observações
+              </Text>
+              <Textarea color="red.900" value={pet.queue?.moreInfos}></Textarea>
+            </Flex>
+          </div>
+          <Flex direction="column" className="div2">
+            <Flex
+              backgroundColor="cyan.100"
+              w="100%"
+              h="48px"
+              direction="row"
+              align="center"
+              justify="center"
+            >
+              <Text mr="1">Exames - </Text>
+              <Text mr="2" color="red">
+                Vermelho: Por fazer{" "}
+              </Text>
+              /
+              <Text ml="2" color="green">
+                Verde: pronto
+              </Text>
+            </Flex>
+
+            <Flex m="2" direction="column" gap="2" overflow="auto">
+              {pet.exams ? (
+                pet.exams.map((exam) => (
+                  <Flex
+                    key={exam.id}
+                    w="100%"
+                    backgroundColor="gray.100"
+                    p="2"
+                    justify="space-between"
+                  >
+                    <>
+                      {exam.doneExam === true ? (
+                        <Text color="green.400" fontWeight="bold">
+                          {exam.name}
+                        </Text>
+                      ) : (
+                        <Text color="red.400" fontWeight="bold">
+                          {exam.name}
+                        </Text>
+                      )}
+                    </>
+                    <Text>{exam.requestedData}</Text>
+                  </Flex>
+                ))
+              ) : (
+                <Text>Sem exame Solicitado</Text>
+              )}
+            </Flex>
           </Flex>
-       
+          <div className="div3">
+            <HStack spacing={4} m="2">
+              <Button colorScheme="whatsapp">Diagnóstico</Button>
+              <Button colorScheme="whatsapp">Prescrição</Button>
+              <Button colorScheme="whatsapp">Sintomas</Button>
+              <Button colorScheme="whatsapp">Solicitar Exame</Button>
+            </HStack>
+            <Textarea
+              border="1px"
+              minHeight={220}
+              m="2"
+              maxWidth={600}
+            ></Textarea>
+          </div>
+          <div className="div4">
+            <Flex justify="space-between" gap="2" m="2">
+              <Button leftIcon={<MdPets />} height={8} colorScheme="whatsapp">
+                Outros Animais
+              </Button>
+              <Button
+                leftIcon={<TbMedicalCrossFilled />}
+                height={8}
+                colorScheme="whatsapp"
+              >
+                Vacinas
+              </Button>
+              <Button
+                leftIcon={<AiFillMedicineBox />}
+                height={8}
+                colorScheme="whatsapp"
+              >
+                Cirurgias
+              </Button>
+            </Flex>
+
+            <Flex m="2" direction="column" gap="2">
+              <Flex
+                w="100%"
+                backgroundColor="gray.100"
+                p="4"
+                justify="space-between"
+              ></Flex>
+
+              <Flex
+                w="100%"
+                backgroundColor="gray.100"
+                p="4"
+                justify="space-between"
+              ></Flex>
+
+              <Flex
+                w="100%"
+                backgroundColor="gray.100"
+                p="4"
+                justify="space-between"
+              ></Flex>
+            </Flex>
+          </div>
+        </WorkSpaceContent>
+        <WorkSpaceFooter>
+          <Flex
+            justify="space-evenly"
+            align="center"
+            width="100%"
+            height="100%"
+          >
+            <Button colorScheme="teal">Imprimir Receita</Button>
+            <Button colorScheme="whatsapp">Imprimir Raio-X</Button>
+            <Button colorScheme="whatsapp">Imprimir Solicitação Exames</Button>
+            <Button colorScheme="red">Gravar Alterações</Button>
           </Flex>
-
-       </div>
-
-      </WorkSpaceContent>
-      <WorkSpaceFooter>
-      <Flex justify="space-evenly" align="center" width="100%" height="100%">
-
-      <Button colorScheme="teal">Imprimir Receita</Button>
-        <Button colorScheme="whatsapp">Imprimir Raio-X</Button>
-        <Button colorScheme="whatsapp">Imprimir Solicitação Exames</Button>
-        <Button colorScheme="red">Gravar Alterações</Button>
-
-      </Flex>
-       <GenericModal isOpen={isModalOpen} onRequestClose={closeModal} >
-        <SetExamForm recordId={pet.recordId}/>
-       </GenericModal>
-      </WorkSpaceFooter>
-    </WorkSpaceContainer>
+          <GenericModal isOpen={isModalOpen} onRequestClose={closeModal}>
+            <SetExamForm recordId={pet.recordId} />
+          </GenericModal>
+        </WorkSpaceFooter>
+      </WorkSpaceContainer>
     </ChakraProvider>
-   
-  )
+  );
 }
