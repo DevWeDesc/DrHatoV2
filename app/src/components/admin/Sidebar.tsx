773d2d3b2657c0b8c2381dc0da828dc5ac2b7e35
DesCTi <desenvolvimento@descti.com.br>
2023-06-30 15:41:18 -0300

    Editando páginas de Adicionar medicamentos e Exames no laboratório

--- app/src/components/admin/Sidebar.tsx
+++ app/src/components/admin/Sidebar.tsx
@@ -1,66 +1,90 @@
-import { Box, Stack, Text, Icon } from "@chakra-ui/react"
-import { RiContactsLine, RiDashboardLine ,TbPigMoney, TbReportAnalytics, GiHealthIncrease, AiOutlineSchedule, GrUnorderedList } from "react-icons/all"
-import { Link } from "react-router-dom"
+import { Box, Stack, Text, Icon } from "@chakra-ui/react";
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  TbPigMoney,
+  TbReportAnalytics,
+  GiHealthIncrease,
+  AiOutlineSchedule,
+  GrUnorderedList,
+} from "react-icons/all";
+import { Link } from "react-router-dom";
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
         <Box>
-          <Text fontWeight="bold" color="gray.700" fontSize="small" >GERAL</Text>
+          <Text fontWeight="bold" color="gray.700" fontSize="30">
+            GERAL
+          </Text>
           <Stack spacing="4" mt="8" align="stretch">
-          <Link to="/Admin/">
-            <Box  display="flex" alignItems="center" color="green.300" > 
-            <Icon as={GrUnorderedList} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Todas opções</Text>
-            </Box>
+            <Link to="/Admin/">
+              <Box display="flex" alignItems="center" color="green.300">
+                <Icon as={GrUnorderedList} fontSize="25" />
+                <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                  Todas opções
+                </Text>
+              </Box>
             </Link>
-
-
 
             <Link to="/Admin/Charts">
-            <Box  display="flex" alignItems="center" color="green.300" > 
-            <Icon as={RiDashboardLine} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Dashboard</Text>
-            </Box>
+              <Box display="flex" alignItems="center" color="green.300">
+                <Icon as={RiDashboardLine} fontSize="25" />
+                <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                  Dashboard
+                </Text>
+              </Box>
             </Link>
-          
-          <Link to="/Users">
-          <Box  display="flex" alignItems="center" color="green.300" > 
-            <Icon as={RiContactsLine} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Usuários</Text>
-            </Box>
-          </Link>
 
-          <Link to="/Vets">
-          <Box  display="flex" alignItems="center" color="green.300" > 
-            <Icon as={GiHealthIncrease} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Veterinários</Text>
-            </Box>
-          </Link>
-          
-          <Link to="/Schedule">
-          <Box  display="flex" alignItems="center" color="green.300" > 
-            <Icon as={AiOutlineSchedule} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Agendar</Text>
-            </Box>
-          </Link>
+            <Link to="/Users">
+              <Box display="flex" alignItems="center" color="green.300">
+                <Icon as={RiContactsLine} fontSize="25" />
+                <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                  Usuários
+                </Text>
+              </Box>
+            </Link>
+
+            <Link to="/Vets">
+              <Box display="flex" alignItems="center" color="green.300">
+                <Icon as={GiHealthIncrease} fontSize="25" />
+                <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                  Veterinários
+                </Text>
+              </Box>
+            </Link>
+
+            <Link to="/Schedule">
+              <Box display="flex" alignItems="center" color="green.300">
+                <Icon as={AiOutlineSchedule} fontSize="25" />
+                <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                  Agendar
+                </Text>
+              </Box>
+            </Link>
           </Stack>
         </Box>
 
         <Box>
-          <Text fontWeight="bold" color="gray.700" fontSize="small" >FINANCEIRO</Text>
+          <Text fontWeight="bold" color="gray.700" fontSize="30">
+            FINANCEIRO
+          </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Box display="flex" alignItems="center" color="green.300" > 
-            <Icon as={TbReportAnalytics} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Relatórios</Text>
+            <Box display="flex" alignItems="center" color="green.300">
+              <Icon as={TbReportAnalytics} fontSize="25" />
+              <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                Relatórios
+              </Text>
             </Box>
-            <Box display="flex" alignItems="center" color="green.300" > 
-            <Icon as={TbPigMoney} fontSize="20"/>
-              <Text ml="4" fontWeight="medium" color="gray.700">Contas</Text>
+            <Box display="flex" alignItems="center" color="green.300">
+              <Icon as={TbPigMoney} fontSize="25" />
+              <Text ml="4" fontWeight="medium" fontSize="25" color="gray.700">
+                Contas
+              </Text>
             </Box>
           </Stack>
         </Box>
       </Stack>
     </Box>
-  )
+  );
 }
--- app/src/pages/Reception/index.tsx
+++ app/src/pages/Reception/index.tsx
@@ -4,17 +4,21 @@ import {
   Box,
   SimpleGrid,
   ChakraProvider,
-  Button
+  Button,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 import { ReceptionSidebar } from "../../components/Sidebars/ReceptionBar";
 import { GridContainer } from "./styles";
+import ImgConsults from "../../assets/ConsultAnimal.jpg";
+import Tools from "../../assets/ImgTools2.png";
+import Pagaments from "../../assets/Pagaments.jpg";
+import Exams from "../../assets/Exams.jpg";
+import Administration from "../../assets/Administration.jpg";
 
 export function Reception() {
   return (
     <ChakraProvider>
-
       <Flex direction="column" h="100vh">
         <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
           <ReceptionSidebar />
@@ -25,103 +29,113 @@ export function Reception() {
             align="flex-start"
             as={Flex}
           >
-            <Box textAlign="center"  p="8" bg="gray.100" borderRadius={8}>
-                <Text fontSize={32} fontWeight="bold" fontStyle="oblique">
-                  RECEPÇÃO GERAL
-                </Text>  
-         
+            <Box textAlign="center" p="8" bg="gray.100" borderRadius={8}>
+              <Text fontSize={32} fontWeight="bold" fontStyle="oblique">
+                RECEPÇÃO GERAL
+              </Text>
+
               <Flex mt="8" justify="center" direction="column">
-               <GridContainer>
-                <div className="section-1">
-                  <div className="container">
-                    <div className="imgContainer">
-                    <img src="https://images.unsplash.com/photo-1519494140681-8b17d830a3e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80" alt="" />
-                    <h1>Consultas</h1>
+                <GridContainer>
+                  <div className="section-1">
+                    <div className="container">
+                      <div className="imgContainer">
+                        <img src={ImgConsults} alt="" />
+                        <h1>Consultas</h1>
+                      </div>
 
-                    </div>
-                   
-                    <div className="buttonsContainer">
-                      <Link to="/Recepcao/Consultas"><Button minWidth={200} colorScheme="whatsapp" >Nova Consulta</Button></Link>
-                      <Link to="/Recepcao/Change">
-                      <Button minWidth={200} colorScheme="whatsapp" >Alterar Consulta</Button>
-                      </Link>
-                    
-                      <Link to="/Recepcao/Create"><Button colorScheme="whatsapp" >Cadastro de Clientes</Button></Link>
+                      <div className="buttonsContainer">
+                        <Link to="/Recepcao/Consultas">
+                          <Button minWidth={200} colorScheme="whatsapp">
+                            Nova Consulta
+                          </Button>
+                        </Link>
+                        <Link to="/Recepcao/Change">
+                          <Button minWidth={200} colorScheme="whatsapp">
+                            Alterar Consulta
+                          </Button>
+                        </Link>
+
+                        <Link to="/Recepcao/Create">
+                          <Button colorScheme="whatsapp">
+                            Cadastro de Clientes
+                          </Button>
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="section-2">
-                <div className="container">
-                    <div className="imgContainer">
-                    <img src="https://images.unsplash.com/photo-1583902340370-6a3fc847d373?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=403&q=80" alt="" />
-                    <h1>Caixas e pagamentos</h1>
+                  <div className="section-2">
+                    <div className="container">
+                      <div className="imgContainer">
+                        <img src={Pagaments} alt="" />
+                        <h1>Caixas e pagamentos</h1>
+                      </div>
 
-                    </div>
-                   
-                    <div className="buttonsContainer">
-                      <Button minWidth={200} colorScheme="whatsapp" >Caixa</Button>
-                      <Button colorScheme="whatsapp" >Reimprimir Caixa</Button>
-                      <Button colorScheme="whatsapp" >Pagamentos</Button>
-                      <Button colorScheme="whatsapp" >Devoluções</Button>
+                      <div className="buttonsContainer">
+                        <Button minWidth={200} colorScheme="whatsapp">
+                          Caixa
+                        </Button>
+                        <Button colorScheme="whatsapp">Reimprimir Caixa</Button>
+                        <Button colorScheme="whatsapp">Pagamentos</Button>
+                        <Button colorScheme="whatsapp">Devoluções</Button>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="section-3">
-                <div className="container">
-                    <div className="imgContainer">
-                    <img src="https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80" alt="" />
-                    <h1>Ferramentas</h1>
+                  <div className="section-3">
+                    <div className="container">
+                      <div className="imgContainer">
+                        <img src={Tools} alt="" />
+                        <h1>Ferramentas</h1>
+                      </div>
 
-                    </div>
-                   
-                    <div className="buttonsContainer">
-                      <Button minWidth={200} colorScheme="whatsapp">Tabela de Preços</Button>
-                      <Button colorScheme="whatsapp" >Trocar Senha</Button>
-                      <Button colorScheme="whatsapp" >Autorizações</Button>
-                      <Button colorScheme="whatsapp" >Medicamentos</Button>
+                      <div className="buttonsContainer">
+                        <Button minWidth={200} colorScheme="whatsapp">
+                          Tabela de Preços
+                        </Button>
+                        <Button colorScheme="whatsapp">Trocar Senha</Button>
+                        <Button colorScheme="whatsapp">Autorizações</Button>
+                        <Button colorScheme="whatsapp">Medicamentos</Button>
+                      </div>
                     </div>
                   </div>
-                  
-                </div>
-                <div className="section-4">
-                <div className="container">
-                    <div className="imgContainer">
-                    <img src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-                    <h1>Internações e Exames</h1>
+                  <div className="section-4">
+                    <div className="container">
+                      <div className="imgContainer">
+                        <img src={Exams} alt="" />
+                        <h1>Internações e Exames</h1>
+                      </div>
 
-                    </div>
-                   
-                    <div className="buttonsContainer">
-                      <Button minWidth={200} colorScheme="whatsapp">Internações</Button>
-                      <Button colorScheme="whatsapp" >Exames</Button>
-                      <Button colorScheme="whatsapp" >Vacinas</Button>
-                    
-                    </div>
-                  </div>
-                </div>
-                <div className="section-5">
-
-                <div className="container">
-                    <div className="imgContainer">
-                    <img src="https://images.unsplash.com/photo-1515847049296-a281d6401047?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
-                    <h1>Administração</h1>
-
-                    </div>
-                   
-                    <div className="buttonsContainer">
-                      <Button minWidth={200} colorScheme="whatsapp">Financeiro</Button>
-                      <Button colorScheme="whatsapp" >Cadastro de Clinicas</Button>
-                      <Button colorScheme="whatsapp" >Cadastro de Raças</Button>
-                    
+                      <div className="buttonsContainer">
+                        <Button minWidth={200} colorScheme="whatsapp">
+                          Internações
+                        </Button>
+                        <Button colorScheme="whatsapp">Exames</Button>
+                        <Button colorScheme="whatsapp">Vacinas</Button>
+                      </div>
                     </div>
                   </div>
-                </div>
+                  <div className="section-5">
+                    <div className="container">
+                      <div className="imgContainer">
+                        <img src={Administration} alt="" />
+                        <h1>Administração</h1>
+                      </div>
 
-               </GridContainer>
+                      <div className="buttonsContainer">
+                        <Button minWidth={200} colorScheme="whatsapp">
+                          Financeiro
+                        </Button>
+                        <Button colorScheme="whatsapp">
+                          Cadastro de Clinicas
+                        </Button>
+                        <Button colorScheme="whatsapp">
+                          Cadastro de Raças
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </GridContainer>
               </Flex>
-
             </Box>
-
           </SimpleGrid>
         </Flex>
       </Flex>
