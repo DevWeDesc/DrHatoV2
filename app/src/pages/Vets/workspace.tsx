36025fa0211b2f535685b1d7e712e911b00916e0
DesCTi <desenvolvimento@descti.com.br>
2023-07-13 15:15:15 -0300

    Dando Pr

--- app/src/pages/Vets/workspace.tsx
+++ app/src/pages/Vets/workspace.tsx
@@ -512,7 +512,7 @@ export function WorkSpaceVet() {
             borderColor="black"
             placeholder="Selecione a Autorização"
           >
-            <option value="option1">Option 1</option>
+            <option value="option1">Option 1 </option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </Select>
