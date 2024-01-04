702b91a641fca9fd205d43e594cc3748a31d8172
ViniHGV <ixxvinicius@gmail.com>
2024-01-04 13:30:35 -0300

    reajust cad customer

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,15 +1,13 @@
-import { FastifyInstance } from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { userController } from '../controllers/userController'
+import { FastifyInstance } from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { userController } from "../controllers/userController";
 
-export async function userRoutes(app: FastifyInstance){
-app.post('/users',userController.createUser ) 
-app.post('/login', userController.loginUser)
-app.get('/vets', userController.findVetUsers)
-app.get('/users', userController.getUsers)
-app.get('/users/:id', userController.getWithId)
-app.put('/users/:id', userController.editUser)
-app.delete('/users/:id' , userController.deleteUser)
+export async function userRoutes(app: FastifyInstance) {
+  app.post("/users", userController.createUser);
+  app.post("/login", userController.loginUser);
+  app.get("/vets", userController.findVetUsers);
+  app.get("/users", userController.getUsers);
+  app.get("/users/:id", userController.getWithId);
+  app.put("/users/:id", userController.editUser);
+  app.delete("/users/:id", userController.deleteUser);
 }
-
-
