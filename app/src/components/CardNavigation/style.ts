acf7c55d014ffb214f556682812d064bd0f0bc0c
ViniHGV <ixxvinicius@gmail.com>
2024-01-10 08:50:26 -0300

    feat: estilização do card container

--- app/src/components/CardNavigation/style.ts
+++ app/src/components/CardNavigation/style.ts
@@ -1,49 +1,44 @@
-import styled
-
-from "styled-components";
+import styled from "styled-components";
 
 export const CardNavigationContainer = styled.div`
-align-items: center;
-justify-content: center;
-width: 320px;
-height: 220px;
-border-radius: 8px;
-display: flex;
-flex-direction: column;
-background-color: white;
-box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 
+  &&:hover {
+    box-shadow: rgba(97, 202, 144, 0.5) 0px 7px 29px 0px;
+  }
 
-&&:hover {
-   box-shadow: rgba(97, 202, 144, 0.5) 0px 7px 29px 0px;
-}
-
-
-
- h2  {
+  h2 {
     font-size: 18px;
     font-weight: bold;
     margin: 1rem;
- }
+  }
 
- p {
+  p {
     margin: 1rem;
- }
-.navBall {
+  }
+  .navBall {
     display: flex;
     align-items: center;
     text-align: center;
     justify-content: center;
-    background-color:  ${props => props.theme['gray-50']};
-    margin:1rem;
+    background-color: ${(props) => props.theme["gray-50"]};
+    margin: 1rem;
     height: 68px;
     width: 68px;
     border-radius: 100%;
-}
-.navContainer, img {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-`
+  }
+  .navContainer,
+  img {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
