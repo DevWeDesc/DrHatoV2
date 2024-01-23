e95c8c809287826eb10947a0c2bd9a6e81d4175a
ViniHGV <ixxvinicius@gmail.com>
2024-01-23 14:52:02 -0300

    feat: movendo rota para procedimentos

--- server/src/routes/exams.routes.ts
+++ server/src/routes/exams.routes.ts
@@ -7,10 +7,6 @@ export async function examsRoutes(app: FastifyInstance) {
     "/petexam/:id/:accId/:examPrice/:linkedDebitId",
     examsController.removePetExam
   );
-  app.delete(
-    "/petprocedure/:id/:accId/:procedurePrice/:linkedDebitId",
-    examsController.removePetProcedure
-  );
   app.get("/exams/old/detail/:examId", examsController.getExamDetailById);
   app.get("/exams/old/all", examsController.getAllExams);
   app.get("/exams/old/:examName", examsController.getByName);
