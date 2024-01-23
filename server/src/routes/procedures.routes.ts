8aa4b543350087404e1cf26c523a26f9a674043b
ViniHGV <ixxvinicius@gmail.com>
2024-01-23 14:52:34 -0300

    style: formatando arquivos

--- server/src/routes/procedures.routes.ts
+++ server/src/routes/procedures.routes.ts
@@ -1,19 +1,36 @@
-import { FastifyInstance } from 'fastify'
-import { proceduresController } from '../controllers/proceduresController'
+import { FastifyInstance } from "fastify";
+import { proceduresController } from "../controllers/proceduresController";
 
 export async function proceduresRoutes(app: FastifyInstance) {
-    app.post('/procedures', proceduresController.createProcedure)
-    app.post('/procedures/:procedureId/:petId/:accId/:queueId', proceduresController.setProcedureInPet)
-    app.get('/procedures', proceduresController.getProcedures)
-    app.get('/procedures/:id', proceduresController.getWithId)
-    app.put('/procedures/:id', proceduresController.editProcedure)
-    app.delete('/procedures/:id', proceduresController.deleteProcedure)
-    app.delete('/proceduresfp/:id/:accId/:procedPrice', proceduresController.deleteProcedureOfPet)
+  app.post("/procedures", proceduresController.createProcedure);
+  app.post(
+    "/procedures/:procedureId/:petId/:accId/:queueId",
+    proceduresController.setProcedureInPet
+  );
+  app.get("/procedures", proceduresController.getProcedures);
+  app.get("/procedures/:id", proceduresController.getWithId);
+  app.put("/procedures/:id", proceduresController.editProcedure);
+  app.delete("/procedures/:id", proceduresController.deleteProcedure);
+  //   app.delete(
+  //     "/proceduresfp/:id/:accId/:procedPrice",
+  //     proceduresController.deleteProcedureOfPet
+  //   );
+  app.delete(
+    "/petprocedure/:id/:accId/:procedurePrice/:linkedDebitId",
+    proceduresController.removePetProcedure
+  );
 
-
-
-    app.get("/procedures/query", proceduresController.queryProcedureByName)
-    app.put("/procedures/especies/:procedureId/:especieId", proceduresController.setEspecieInProcedure)
-    app.put("/procedures/especies/all/:procedureId", proceduresController.setAllEspeciesInProcedure)
-    app.put("/procedures/especies/all/remove/:procedureId", proceduresController.removeAllEspeciesInProcedure)
+  app.get("/procedures/query", proceduresController.queryProcedureByName);
+  app.put(
+    "/procedures/especies/:procedureId/:especieId",
+    proceduresController.setEspecieInProcedure
+  );
+  app.put(
+    "/procedures/especies/all/:procedureId",
+    proceduresController.setAllEspeciesInProcedure
+  );
+  app.put(
+    "/procedures/especies/all/remove/:procedureId",
+    proceduresController.removeAllEspeciesInProcedure
+  );
 }
