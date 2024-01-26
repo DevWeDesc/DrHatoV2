ff6fc1258618c9292d91d64db164c91102b7694b
ViniHGV <ixxvinicius@gmail.com>
2024-01-26 14:56:13 -0300

    style: formatando SearchEngine

--- server/src/engines/searchEngine.ts
+++ server/src/engines/searchEngine.ts
@@ -1,49 +1,105 @@
 import { prisma } from "../interface/PrismaInstance";
 interface SearchEngineProps {
-        customerName?: string;
-         initialDate?: Date; 
-           finalDate?: Date; 
-              codPet?: string; 
-             petName?: string; 
-      isHospitalized?: boolean | string; 
-          isFinished?: boolean | string;
+  customerName?: string;
+  initialDate?: Date;
+  finalDate?: Date;
+  codPet?: string;
+  petName?: string;
+  isHospitalized?: boolean | string;
+  isFinished?: boolean | string;
+  typePaymentClient?: string;
 }
-export async function searchEngine({customerName,petName,codPet,finalDate,initialDate,isFinished,isHospitalized} : SearchEngineProps) {
-    let responseArray: any = [];
-    switch(true) {                                               
-        case !!customerName && !!petName: 
-        const customerAndPetsResults = await prisma.customer.findMany(
-            {where: {AND: [
-            {name: {startsWith: customerName}},
-            {pets: {some: {name: {startsWith: petName}}}}
-        ]}, include: {pets: true}})
-        responseArray = responseArray.concat(customerAndPetsResults)
-        break;
+export async function searchEngine({
+  customerName,
+  petName,
+  codPet,
+  finalDate,
+  initialDate,
+  isFinished,
+  isHospitalized,
+  typePaymentClient,
+}: SearchEngineProps) {
+  let responseArray: any = [];
+  switch (true) {
+    case !!customerName && !!petName:
+      const customerAndPetsResults = await prisma.customer.findMany({
+        where: {
+          AND: [
+            { name: { startsWith: customerName } },
+            { pets: { some: { name: { startsWith: petName } } } },
+          ],
+        },
+        include: { pets: true },
+      });
+      responseArray = responseArray.concat(customerAndPetsResults);
+      break;
 
-        case !!customerName: 
-        const customerResults =  await prisma.pets.findMany({where: {customer :{name :{startsWith: customerName}}}, include: {customer: true}})
-        responseArray = responseArray.concat(customerResults)
-        break;
+    case !!customerName:
+      const customerResults = await prisma.pets.findMany({
+        where: { customer: { name: { startsWith: customerName } } },
+        include: { customer: true },
+      });
+      responseArray = responseArray.concat(customerResults);
+      break;
 
-        case !!petName:
-        const petResults = await prisma.pets.findMany({where: {name: {startsWith: petName}}, include:{ customer: true}})
-        responseArray = responseArray.concat(petResults)
-        break; 
+    case !!petName:
+      const petResults = await prisma.pets.findMany({
+        where: { name: { startsWith: petName } },
+        include: { customer: true },
+      });
+      responseArray = responseArray.concat(petResults);
+      break;
 
-        case !!codPet: 
-        const codPetResulst = await prisma.pets.findFirst({where: {codPet: {startsWith: codPet}},include:{ customer: true}})
-        responseArray = responseArray.concat(codPetResulst)
-        break;
+    case !!codPet:
+      const codPetResulst = await prisma.pets.findFirst({
+        where: { codPet: { startsWith: codPet } },
+        include: { customer: true },
+      });
+      responseArray = responseArray.concat(codPetResulst);
+      break;
 
-        case !!isFinished:
-        const isFinishedResults = await prisma.queuesForPet.findMany({
-            where: {queueIsDone: true}, include:{medicine: {include: {pet: {select: {name: true, id: true, customer: {select: {name: true, cpf: true}}}}}}}
-        })    
-        responseArray = responseArray.concat(isFinishedResults)
-        break;
-    }
-    
-    if(responseArray.length <= 0) return null;
+    case !!isFinished:
+      const isFinishedResults = await prisma.queuesForPet.findMany({
+        where: { queueIsDone: true },
+        include: {
+          medicine: {
+            include: {
+              pet: {
+                select: {
+                  name: true,
+                  id: true,
+                  customer: { select: { name: true, cpf: true } },
+                },
+              },
+            },
+          },
+        },
+      });
+      responseArray = responseArray.concat(isFinishedResults);
+      break;
 
-    return responseArray
+    case !!typePaymentClient:
+      const IstypePaymentClientResult = await prisma.queuesForPet.findMany({
+        where: { queueIsDone: true },
+        include: {
+          medicine: {
+            include: {
+              pet: {
+                select: {
+                  name: true,
+                  id: true,
+                  customer: { select: { name: true, cpf: true } },
+                },
+              },
+            },
+          },
+        },
+      });
+      responseArray = responseArray.concat(IstypePaymentClientResult);
+      break;
+  }
+
+  if (responseArray.length <= 0) return null;
+
+  return responseArray;
 }
