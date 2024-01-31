4b795aad4ac6e0752bc0c969f5674fc08c087c8f
ViniHGV <ixxvinicius@gmail.com>
2024-01-31 14:11:41 -0300

    feat: adicionando lógica para parte de documentos do caixa, e listando todos os pagamentos

--- app/src/components/Box/receveidsDocuments.tsx
+++ app/src/components/Box/receveidsDocuments.tsx
@@ -1,106 +1,107 @@
-import { Flex, Heading, TableContainer,Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
+import { BoxContext } from "../../contexts/BoxContext";
+import { api } from "../../lib/axios";
+import { IDocBox } from "../../interfaces";
+import { useNavigate } from "react-router-dom";
 
-export function ReceveidDocuments() {
-    return (
-        <>
-         <Flex
-            justify="space-between"
-            direction="column"
-            align="center"
-          >
-            <Heading
-              bg="gray.700"
-              color="white"
-              size="lg"
-              fontWeight="bold"
-              w="100%"
-              textAlign="center"
-              py="5"
-            >
-              Documentos Recebidos
-            </Heading>
-          </Flex>
-  
-          <TableContainer>
-            <Table variant="striped" colorScheme="teal">
-              <Thead>
-                <Tr>
-                  <Th border="1px solid black" fontSize="17">
-                    Tipo
-                  </Th>
-                  <Th border="1px solid black" fontSize="17">
-                    Cliente
-                  </Th>
-                  <Th border="1px solid black" fontSize="17" isNumeric>
-                    Valor
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td style={{ border: "1px solid black" }}>
-                    Depósito Bancário
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    fontWeight="bold"
-                  >
-                    Dilan Lopez
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    isNumeric
-                    fontWeight="bold"
-                  >
-                    60,00
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td style={{ border: "1px solid black" }}>
-                    Dinheiro
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    fontWeight="bold"
-                  >
-                    Djalma Rodrigues dos Santos
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    isNumeric
-                    fontWeight="bold"
-                  >
-                    327,00
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td style={{ border: "1px solid black" }}>
-                    Master Card Débito
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    fontWeight="bold"
-                  >
-                    Leiliane Pereira Do Nascimento
-                  </Td>
-                  <Td
-                    style={{ border: "1px solid black" }}
-                    border="1px solid black"
-                    isNumeric
-                    fontWeight="bold"
-                  >
-                    60,00
-                  </Td>
-                </Tr>
-                
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </>
-    )
+interface ReceveidDocumentsProps {
+  refresh: boolean;
+  handleRefresh: () => void;
+}
+
+export function ReceveidDocuments({
+  refresh,
+  handleRefresh,
+}: ReceveidDocumentsProps) {
+  const { fatherBox } = useContext(BoxContext);
+  const [documents, setDocuments] = useState({} as IDocBox);
+  const navigate = useNavigate();
+
+  const requestDocumentsByAPi = async () => {
+    let response = await api.get(`/account/debitByBox/${fatherBox.id}`);
+    setDocuments(response.data);
+  };
+
+  useEffect(() => {
+    if (refresh) {
+      requestDocumentsByAPi();
+      handleRefresh();
+    }
+  }, [refresh]);
+
+  // console.log(documents);
+
+  return (
+    <>
+      <Flex justify="space-between" direction="column" align="center">
+        <Heading
+          bg="gray.700"
+          color="white"
+          size="lg"
+          fontWeight="bold"
+          w="100%"
+          textAlign="center"
+          py="5"
+        >
+          Documentos Recebidos
+        </Heading>
+      </Flex>
+
+      <TableContainer>
+        <Table variant="striped" colorScheme="teal">
+          <Thead>
+            <Tr>
+              <Th border="1px solid black" fontSize="17">
+                Tipo
+              </Th>
+              <Th border="1px solid black" fontSize="17">
+                Cliente
+              </Th>
+              <Th border="1px solid black" fontSize="17" isNumeric>
+                Valor
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {documents?.Installments?.map((doc) => (
+              <Tr
+                key={doc.id}
+                onClick={() =>
+                  navigate(`/Recepcao/Caixa/Returns/${doc.customerId}`)
+                }
+              >
+                <Td style={{ border: "1px solid black" }}>{doc.paymentType}</Td>
+                <Td
+                  style={{ border: "1px solid black" }}
+                  border="1px solid black"
+                  fontWeight="bold"
+                >
+                  {doc.customerAccount.customer.name}
+                </Td>
+                <Td
+                  style={{ border: "1px solid black" }}
+                  border="1px solid black"
+                  isNumeric
+                  fontWeight="bold"
+                >
+                  {doc.totalDebit.toString().concat(",00")}
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
