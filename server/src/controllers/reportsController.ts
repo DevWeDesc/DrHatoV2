d557c45b926cda511748ed53dbb3da21df1aa7cf
ViniHGV <ixxvinicius@gmail.com>
2024-02-06 14:14:23 -0300

    feat: Finalizando EndPoint com algumas adições

--- server/src/controllers/reportsController.ts
+++ server/src/controllers/reportsController.ts
@@ -2,49 +2,52 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import prisma from "../../client";
 
-
-
-
-
-
-
-
 export const reportsController = {
-  reportBySector: async function (request: FastifyRequest, reply: FastifyReply) {
+  reportBySector: async function (
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) {
     try {
       const ReportBySectorSchema = z.object({
-        initialDate: z.date(),
-        finalDate: z.date()
-      })
+        initialDate: z.string(),
+        finalDate: z.string(),
+      });
 
-      const {initialDate, finalDate} = ReportBySectorSchema.parse(request.body)
+      const { initialDate, finalDate } = ReportBySectorSchema.parse(
+        request.body
+      );
 
       const consult = await prisma.openedConsultsForPet.findMany({
         where: {
           AND: [
-            {openedDate: {gte: initialDate}},
-            {closedDate: {lt: finalDate}}
-          ]
+            { openedDate: { gte: new Date(initialDate) } },
+            { closedDate: { lt: new Date(finalDate) } },
+          ],
         },
         include: {
-          consultDebits: true
-        }
-      })
+          consultDebits: true,
+        },
+      });
 
       const proceduresByConsults = consult.flatMap((consults) => {
-        return consults.consultDebits.filter((c) => c.sectorId === 1)
-      })
+        return consults.consultDebits.filter(
+          (c) => Number(c.consultOpenedId) !== 1
+        );
+      });
 
       const data = {
         consults: {
           procedures: proceduresByConsults,
           consultsQuantity: proceduresByConsults.length,
-          consultsInvoicing: proceduresByConsults.reduce((accumulator, currentValue) => accumulator + Number(currentValue.price), 0)
-        }
-      }
+          consultsInvoicing: proceduresByConsults.reduce(
+            (accumulator, currentValue) =>
+              accumulator + Number(currentValue.price),
+            0
+          ),
+        },
+      };
 
-    } catch (error) {
-      
-    }
-  }
-}
+      return reply.status(200).send(data);
+    } catch (error) {}
+  },
+};
