1c25b76ba1736d4ed5919f369a59df7067545d2a
ViniHGV <ixxvinicius@gmail.com>
2024-02-07 14:59:03 -0300

    feat: criando rota para solicitar requisições para a controller reportExamsConclused

--- server/src/routes/reports.routes.ts
+++ server/src/routes/reports.routes.ts
@@ -3,4 +3,5 @@ import { reportsController } from "../controllers/reportsController";
 
 export async function reportsRoutes(app: FastifyInstance) {
   app.post("/reports/sector", reportsController.reportBySector);
+  app.post("/reports/exams", reportsController.reportExamsConclused);
 }
