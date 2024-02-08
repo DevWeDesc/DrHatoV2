32ca15721e1d70a0db7f0b140a9e7f69f13bbab7
ViniHGV <ixxvinicius@gmail.com>
2024-02-08 14:29:35 -0300

    feat: Editando exibição de modal para adição de exames

--- app/src/components/administration/Exams/index.tsx
+++ app/src/components/administration/Exams/index.tsx
@@ -12,63 +12,60 @@ import {
   Thead,
   Tr,
   FormControl,
-  HStack,
-  CheckboxGroup,
   Checkbox,
+  TableContainer,
+  Select,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { LoadingSpinner } from "../../../components/Loading";
 import { GenericModal } from "../../../components/Modal/GenericModal";
 import { api } from "../../../lib/axios";
 import { toast } from "react-toastify";
 import { Input } from "../../../components/admin/Input";
 import { ConfirmationDialog } from "../../dialogConfirmComponent/ConfirmationDialog";
 import { BsFillTrashFill } from "react-icons/bs";
-import { NewCharacteristics } from "./newCharacteristics"; // DELETAR
 import { useQuery, useQueryClient, useMutation } from "react-query";
 
-
-type ExamsDTO = {  
+type ExamsDTO = {
   codexam: number;
   name: string;
   price: number;
   disponible: boolean;
   onePart: boolean;
-	twoPart: boolean;
-	byReport: boolean;
-}
+  twoPart: boolean;
+  byReport: boolean;
+};
 
 type CreateExamsDTO = {
   name: string;
   price: number;
   available: boolean;
-  examsType: any
-}
+  examsType: any;
+};
 
 export function ListExams() {
   const { register, handleSubmit } = useForm();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [exams, setExams] = useState<ExamsDTO[]>([]);
 
-  async function getExamesListData () {
+  async function getExamesListData() {
     const exams = await api.get("/exams/old/all");
     setExams(exams.data.exams);
-  };
+  }
 
-  const queryClient = useQueryClient()
-  const {isLoading, error} = useQuery('adminExams', () => getExamesListData)
-  const {mutate} = useMutation(
-    (data: CreateExamsDTO) => api.post('exams', data),
+  const queryClient = useQueryClient();
+  const { isLoading, error } = useQuery("adminExams", () => getExamesListData);
+  const { mutate } = useMutation(
+    (data: CreateExamsDTO) => api.post("exams", data),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries('adminExams');
-        toast.success('Exame criado com sucesso');
+        queryClient.invalidateQueries("adminExams");
+        toast.success("Exame criado com sucesso");
       },
       onError: (error) => {
-        toast.error('Falha ao criar novo Exame');
+        toast.error("Falha ao criar novo Exame");
         console.error(error);
       },
     }
@@ -82,12 +79,10 @@ export function ListExams() {
         available: values.available,
         examsType: values.examsType,
       });
-
     } catch (error) {
       console.error(error);
     }
   };
-
 
   async function DeleteExam(ExamId: string) {
     await api
@@ -139,7 +134,7 @@ export function ListExams() {
             <Th fontSize="21" borderColor="black">
               Duas Partes
             </Th>
-           
+
             <Th width="8" borderColor="black"></Th>
             <Th width="8" borderColor="black"></Th>
           </Tr>
@@ -147,111 +142,203 @@ export function ListExams() {
 
         <Tbody>
           {exams.map((exam) => (
-              <Tr key={exam.codexam} fontSize="18">
-                <Td borderColor="black">
-                  <Box>
-                    <Link to={`/Admin/Exams/Details/${exam.codexam}`}>
-                      <Text
-                        fontWeight="bold"
-                        color="gray.800"
-                        borderColor="black"
-                      >
-                        {exam.name}
-                      </Text>
-                    </Link>
-                  </Box>
-                </Td>
-                <Td borderColor="black">
-                  <Text fontWeight="bold" color="gray.800">
-                    {new Intl.NumberFormat("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    }).format(exam.price)}
-                  </Text>
-                </Td>
-                <Td  
+            <Tr key={exam.codexam} fontSize="18">
+              <Td borderColor="black">
+                <Box>
+                  <Link to={`/Admin/Exams/Details/${exam.codexam}`}>
+                    <Text
+                      fontWeight="bold"
+                      color="gray.800"
+                      borderColor="black"
+                    >
+                      {exam.name}
+                    </Text>
+                  </Link>
+                </Box>
+              </Td>
+              <Td borderColor="black">
+                <Text fontWeight="bold" color="gray.800">
+                  {new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(exam.price)}
+                </Text>
+              </Td>
+              <Td
                 fontWeight="bold"
                 color="gray.800"
                 borderColor="black"
                 bgColor={exam.disponible ? "green.200" : "red.100"}
-                >
-                        
-                  {exam.disponible ? "SIM" : "NÃO"}
-                </Td>
-                <Td  fontWeight="bold"
-                        color="gray.800"
-                        borderColor="black">{exam.twoPart ? "SIM" : "NÃO"}</Td>
-                <Td borderColor="black">
-                  {" "}
-                  <ConfirmationDialog
-                    disabled={false}
-                    icon={<BsFillTrashFill fill="white" size={16} />}
-                    buttonTitle="Deletar Exame"
-                    whatIsConfirmerd="Tem certeza que deseja Excluir esse Exame?"
-                    describreConfirm="Excluir a Exame é uma ação irreversivel, tem certeza que deseja excluir?"
-                    callbackFn={() => DeleteExam(exam.codexam.toString())}
-                  />
-                </Td>
-                <Td borderColor="black">
-                  <Link to={`/Admin/Exams/${exam.codexam}`}>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="yellow"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    >
-                      Configurar
-                    </Button>
-                  </Link>
-                </Td>
-              </Tr>
-            ))}
+              >
+                {exam.disponible ? "SIM" : "NÃO"}
+              </Td>
+              <Td fontWeight="bold" color="gray.800" borderColor="black">
+                {exam.twoPart ? "SIM" : "NÃO"}
+              </Td>
+              <Td borderColor="black">
+                <ConfirmationDialog
+                  disabled={false}
+                  icon={<BsFillTrashFill fill="white" size={16} />}
+                  buttonTitle="Deletar Exame"
+                  whatIsConfirmerd="Tem certeza que deseja Excluir esse Exame?"
+                  describreConfirm="Excluir a Exame é uma ação irreversivel, tem certeza que deseja excluir?"
+                  callbackFn={() => DeleteExam(exam.codexam.toString())}
+                />
+              </Td>
+              <Td borderColor="black">
+                <Link to={`/Admin/Exams/${exam.codexam}`}>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="yellow"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Configurar
+                  </Button>
+                </Link>
+              </Td>
+            </Tr>
+          ))}
         </Tbody>
       </Table>
-      <GenericModal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}>
+      <GenericModal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+      >
         <FormControl as="form" onSubmit={handleSubmit(handleCreateExam)}>
-          <Flex direction="column" align="center" margin="4">
-            <Input {...register("name")} name="name" label="Nome do Exame" />
-            <Input {...register("price")} name="price" label="Preço" />
-
-            <HStack gap="2" margin={8}>
-              <label htmlFor="available">Disponivel ?</label>
-              <Checkbox
-                {...register("available")}
-                id="available"
-                name="available"
-                type="checkbox"
-                _checked={{ background: "#FF0000" }}
-              />
-            </HStack>
-
-            <HStack spacing={4} margin="4">
-              <CheckboxGroup>
-                <label htmlFor="">Lab Imagens</label>
+          <TableContainer>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th colSpan={2} textAlign="center">
+                    Cadastro de Exames
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td py={1}>Nome do Exame</Td>
+                  <Td py={1}>
+                    <Input {...register("name")} name="name" />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td py={1}>Preço</Td>
+                  <Td py={1}>
+                    <Input {...register("price")} name="price" />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td py={1}>Faixas Etárias</Td>
+                  <Td py={1}>
+                    <Input name="" />
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td py={1}>Setor</Td>
+                  <Td py={1}>
+                    <Select
+                      borderColor="blackAlpha.800"
+                      _hover={{
+                        backgroundColor: "gray.100",
+                        borderColor: "blackAlpha.800",
+                      }}
+                    >
+                      <option value="Veterinário">Veterinário</option>
+                      <option value="Laboratório">Laboratório</option>
+                      <option value="Internação">Internação</option>
+                    </Select>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th colSpan={2} textAlign="center">
+                    Uma Parte
+                  </Th>
+                  <Th colSpan={2} textAlign="center">
+                    Duas Partes
+                  </Th>
+                  <Th colSpan={2} textAlign="center">
+                    Formato de Laudo
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Th colSpan={2} textAlign="center">
+                    <Checkbox borderColor="blackAlpha.500" />
+                  </Th>
+                  <Th colSpan={2} textAlign="center">
+                    <Checkbox borderColor="blackAlpha.500" />
+                  </Th>
+                  <Th colSpan={2} textAlign="center">
+                    <Checkbox borderColor="blackAlpha.500" />
+                  </Th>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+          {/* <Flex direction="column" align="center" margin="4"> */}
+          {/* <Input {...register("price")} name="price" label="Preço" /> */}
+          {/* <Flex direction="column" gap={4} my={2}>
+              <HStack gap="2">
+                <label htmlFor="available">Formato de Laudo ?</label>
                 <Checkbox
-                  {...register("examsType")}
-                  value={"image"}
-                  type="radio"
-                  colorScheme="green"
-                  name="examsType"
+                  {...register("available")}
+                  id="available"
+                  name="available"
+                  type="checkbox"
+                  _checked={{ background: "#FF0000" }}
                 />
+              </HStack>
 
-                <label htmlFor="">Labs</label>
+              <HStack gap="2">
+                <label htmlFor="available">Disponivel ?</label>
                 <Checkbox
-                  type="radio"
-                  {...register("examsType")}
-                  value={"lab"}
-                  colorScheme="green"
-                  name="examsType"
+                  {...register("available")}
+                  id="available"
+                  name="available"
+                  type="checkbox"
+                  _checked={{ background: "#FF0000" }}
                 />
-              </CheckboxGroup>
-            </HStack>
+              </HStack>
+              <HStack spacing={4}>
+                <CheckboxGroup>
+                  <label htmlFor="">Lab Imagens</label>
+                  <Checkbox
+                    {...register("examsType")}
+                    value={"image"}
+                    type="radio"
+                    colorScheme="green"
+                    name="examsType"
+                  />
 
-            <Button type="submit" colorScheme="green" m="2" isLoading={isLoading}>
-                    Cadastrar
-            </Button>
-          </Flex>
+                  <label htmlFor="">Labs</label>
+                  <Checkbox
+                    type="radio"
+                    {...register("examsType")}
+                    value={"lab"}
+                    colorScheme="green"
+                    name="examsType"
+                  />
+                </CheckboxGroup>
+              </HStack>
+            </Flex> */}
+
+          {/* </Flex> */}
+          <Button
+            width="100%"
+            type="submit"
+            colorScheme="green"
+            m="2"
+            isLoading={isLoading}
+          >
+            Cadastrar
+          </Button>
         </FormControl>
       </GenericModal>
     </Box>
