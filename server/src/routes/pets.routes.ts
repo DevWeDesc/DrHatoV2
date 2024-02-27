ecde3313aca2e8ce932249b0092864eb71b47fd8
ViniHGV <ixxvinicius@gmail.com>
2024-02-27 13:13:52 -0300

    feat: adicionando rotas para results old

--- server/src/routes/pets.routes.ts
+++ server/src/routes/pets.routes.ts
@@ -1,25 +1,28 @@
-import { FastifyInstance, } from 'fastify'
-import { petsController } from '../controllers/petsController'
-
+import { FastifyInstance } from "fastify";
+import { petsController } from "../controllers/petsController";
 
 export async function petRoutes(app: FastifyInstance) {
+  app.get("/pets", petsController.getAllPets);
+  app.get("/pets/:id", petsController.getWithId);
+  app.get("/petsall/:petId", petsController.getPetBedHistory);
+  app.post("/pets/:id", petsController.createPet);
+  app.get("/pets/queue", petsController.petsInQueue);
+  app.get("/pets/queue/preference/:vetName", petsController.petsByVetQueue);
+  app.put("/pet/:petId/:weight", petsController.changePetWeight);
 
-  app.get('/pets', petsController.getAllPets)
-  app.get('/pets/:id', petsController.getWithId)
-  app.get('/petsall/:petId', petsController.getPetBedHistory)
-  app.post('/pets/:id', petsController. createPet)
-  app.get('/pets/queue', petsController.petsInQueue)
-  app.get('/pets/queue/preference/:vetName', petsController.petsByVetQueue)
-  app.put('/pet/:petId/:weight', petsController.changePetWeight)
+  app.post("/pets/especie", petsController.createEspecie);
+  app.get("/pets/especie", petsController.getEspecies);
+  app.get("/pets/especie/:espId", petsController.getEspeciesById);
+  app.post("/pets/races", petsController.createRaces);
 
+  app.get("/pets/history/:petId", petsController.getAllPetHistory);
+  app.get(
+    "/pet/old/history/consults/:petId",
+    petsController.getPetOldHistoryConsults
+  );
 
-  app.post('/pets/especie', petsController.createEspecie)
-  app.get('/pets/especie', petsController.getEspecies)
-  app.get('/pets/especie/:espId', petsController.getEspeciesById)
-  app.post('/pets/races', petsController.createRaces)
-
-
-  app.get('/pets/history/:petId', petsController.getAllPetHistory)
-  app.get('/pet/old/history/consults/:petId', petsController.getPetOldHistoryConsults)
-
+  app.get("/pet/results/old", petsController.getAllResultsOld);
+  app.get("/pet/results/old/:petId", petsController.getResultsOldByPet);
+  // app.get("/pet/results/old/:examId/:petId", petsController.getResultOldByExamId);
+  app.post("/pet/results/old/:examId/:petId", petsController.createResultsOld);
 }
