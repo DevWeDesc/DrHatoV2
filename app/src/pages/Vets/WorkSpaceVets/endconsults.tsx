02b2d48e5ebd57279c1cc2032643a36b3305df2e
ViniHGV <ixxvinicius@gmail.com>
2024-03-19 13:43:56 -0300

    feat: formatando finalização de consulta e adicionando desconto

--- app/src/pages/Vets/WorkSpaceVets/endconsults.tsx
+++ app/src/pages/Vets/WorkSpaceVets/endconsults.tsx
@@ -1,15 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import { Button, Flex, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
-import { useParams } from 'react-router-dom';
-import { api } from '../../../lib/axios';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Flex,
+  Input,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { api } from "../../../lib/axios";
+import { toast } from "react-toastify";
 interface ConsultDebitsProps {
   id: string;
   openedDate: Date;
   consultType: string;
   medicineRecordId: number;
-  PetConsultsDebits: Array< {
-    id:number;
+  PetConsultsDebits: Array<{
+    id: number;
     name: string;
     price: number;
     RequestedByVetName: string;
@@ -19,19 +31,19 @@ interface ConsultDebitsProps {
     isVaccine: boolean;
     isProcedure: boolean;
     isAdmission: boolean;
-    }>
-  consultDebits: Array< {
-  id:number;
-  name: string;
-  price: number;
-  RequestedByVetName: string;
-  requestedDate: Date;
-  isExam: boolean;
-	isSurgerie: boolean;
-	isVaccine: boolean;
-	isProcedure: boolean;
-	isAdmission: boolean;
-  }>
+  }>;
+  consultDebits: Array<{
+    id: number;
+    name: string;
+    price: number;
+    RequestedByVetName: string;
+    requestedDate: Date;
+    isExam: boolean;
+    isSurgerie: boolean;
+    isVaccine: boolean;
+    isProcedure: boolean;
+    isAdmission: boolean;
+  }>;
 }
 
 interface EndConsutsProps {
@@ -39,92 +51,192 @@ interface EndConsutsProps {
   handleCloseAdmission?: () => void;
   isAdmission?: boolean;
 }
-export function EndConsults({handleCloseQuery, handleCloseAdmission, isAdmission}: EndConsutsProps) {
+export function EndConsults({
+  handleCloseQuery,
+  handleCloseAdmission,
+  isAdmission,
+}: EndConsutsProps) {
   const { id, queueId } = useParams<{ id: string; queueId: string }>();
-  const [consultDebitsDetails, setConsultDebitsDetails] = useState<ConsultDebitsProps[]>([])
-  const [totalDebits, setTotalDebits] = useState({ total: 0});
+  const [consultDebitsDetails, setConsultDebitsDetails] = useState<
+    ConsultDebitsProps[]
+  >([]);
+  const [totalDebits, setTotalDebits] = useState({
+    total: 0,
+    totalWithDiscont: 0,
+  });
+  const [discount, setDiscount] = useState<any>("");
+  const [quantityMaxDiscont, setQuantityMaxDiscont] = useState(0);
 
   async function getConsultDebitsDetails() {
     try {
-      if(isAdmission === true) {
-        const response = await api.get(`/debits/pets/admissions/${queueId}`)
-        setConsultDebitsDetails(response.data.debits)
-        setTotalDebits(response.data.total)
+      if (isAdmission === true) {
+        const response = await api.get(`/debits/pets/admissions/${queueId}`);
+        setConsultDebitsDetails(response.data.debits);
+        setTotalDebits(response.data.total);
       } else {
-        const response = await api.get(`/debits/pets/consults/${queueId}`)
-        setConsultDebitsDetails(response.data.debits)
-        setTotalDebits(response.data.total)
+        const response = await api.get(`/debits/pets/consults/${queueId}`);
+        setConsultDebitsDetails(response.data.debits);
+        setTotalDebits(response.data.total);
       }
-
     } catch (error) {
-      toast.error("Falha ao buscar informações da consulta!")
+      toast.error("Falha ao buscar informações da consulta!");
     }
   }
 
-    useEffect(() => {
-      getConsultDebitsDetails()
-    }, [])
+  const calculateDiscont = () => {
+    if (quantityMaxDiscont < 1) {
+      const debitsWithDiscont =
+        discount > 0
+          ? totalDebits.total - (totalDebits.total * parseFloat(discount)) / 100
+          : totalDebits.total;
+      setTotalDebits({
+        ...totalDebits,
+        totalWithDiscont: debitsWithDiscont,
+      });
+    } else {
+      toast.warning(`já foi aplicado o desconto de ${discount}%`);
+    }
+    setQuantityMaxDiscont((prev) => prev + 1);
+  };
 
+  const removeDiscont = () => {
+    setDiscount("");
+    setTotalDebits({ ...totalDebits, totalWithDiscont: 0 });
+    setQuantityMaxDiscont(0);
+  };
 
-    console.log("GETs", consultDebitsDetails)
+  useEffect(() => {
+    getConsultDebitsDetails();
+  }, []);
+
+  console.log("GETs", consultDebitsDetails);
 
   return (
-   <Flex w="800px" h="600px" direction="column" >  
-       {
-        consultDebitsDetails.map((consults) => (
-          <React.Fragment key={consults.id}>
-          <Flex m="2" align="center" justifyContent="space-between">  
-            <Text fontWeight="bold" fontSize="lg">{!isAdmission ? `Consulta iniciada` : `Internação iniciada`  } em: {new Intl.DateTimeFormat('pt-BR', {day: '2-digit', month: '2-digit',
-          hour: '2-digit', minute: '2-digit'}).format(new Date(consults?.openedDate))}</Text>
-          <Text fontWeight="bold" fontSize="lg">{ !isAdmission ? `Tipo de consulta: ${consults?.consultType}` : `Internação`  }</Text>
-          </Flex> 
-          <TableContainer overflowY="scroll" w="100%" mt="4" h={540} >
-            <Table >
-            <Thead>
-              <Tr>
-                <Th fontSize="lg">Item</Th>
-                <Th fontSize="lg">Valor</Th>
-                <Th fontSize="lg">Data</Th>
-                <Th fontSize="lg">Solicitado Por</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-                {
-                  !isAdmission ? consults?.consultDebits?.map((item) => (
-                    <Tr key={item.id}>
-                     <Td>{item.name}</Td>
-                     <Td>{new Intl.NumberFormat('pt-BR', {
-                      currency: 'BRL',
-                      style: 'currency'
-                     }).format(item.price)}</Td>
-                     <Td>{new Intl.DateTimeFormat('pt-BR', {day: '2-digit', month: '2-digit',
-          hour: '2-digit', minute: '2-digit'}).format(new Date(item?.requestedDate))}</Td>
-                                <Td>{item.RequestedByVetName}</Td>
-                    </Tr>
-                  )) :     consults?.consultDebits?.map((item) => (
-                    <Tr key={item.id}>
-                     <Td>{item.name}</Td>
-                     <Td>{new Intl.NumberFormat('pt-BR', {
-                      currency: 'BRL',
-                      style: 'currency'
-                     }).format(item.price)}</Td>
-                     <Td>{new Intl.DateTimeFormat('pt-BR', {day: '2-digit', month: '2-digit',
-          hour: '2-digit', minute: '2-digit'}).format(new Date(item?.requestedDate))}</Td>
-                                <Td>{item.RequestedByVetName}</Td>
-                    </Tr>
-                  ))
-                }
+    <Flex w="800px" h="600px" direction="column">
+      {consultDebitsDetails.map((consults) => (
+        <React.Fragment key={consults.id}>
+          <Flex m="2" align="center" justifyContent="space-between">
+            <Text fontWeight="bold" fontSize="lg">
+              {!isAdmission ? `Consulta iniciada` : `Internação iniciada`} em:{" "}
+              {new Intl.DateTimeFormat("pt-BR", {
+                day: "2-digit",
+                month: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+              }).format(new Date(consults?.openedDate))}
+            </Text>
+            <Text fontWeight="bold" fontSize="lg">
+              {!isAdmission
+                ? `Tipo de consulta: ${consults?.consultType}`
+                : `Internação`}
+            </Text>
+          </Flex>
+          <TableContainer overflowY="scroll" w="100%" mt="4" h={540}>
+            <Table>
+              <Thead>
+                <Tr bg="blue.400">
+                  <Th colSpan={1} textColor="white" fontSize="md">
+                    {isAdmission
+                      ? "Custos desta Internação"
+                      : "Custos desta Consulta"}
+                  </Th>
+                  <Th colSpan={3} textColor="white" fontSize="md">
+                    <Flex justifyContent="end" gap={2} align="end">
+                      <Flex direction="column" align="center">
+                        <Text w="full" fontSize="sm">
+                          Desconto
+                        </Text>
+                        <Input
+                          value={discount}
+                          onChange={(ev) => setDiscount(ev.target.value)}
+                          bg="white"
+                          textColor="black"
+                        />
+                      </Flex>
+                      <Button colorScheme="whatsapp" onClick={calculateDiscont}>
+                        Calcular
+                      </Button>
+                      <Button colorScheme="red" onClick={removeDiscont}>
+                        Remover Desconto
+                      </Button>
+                    </Flex>
+                  </Th>
+                </Tr>
 
-            </Tbody>
-
+                <Tr>
+                  <Th fontSize="md">Item</Th>
+                  <Th fontSize="md">Valor</Th>
+                  <Th fontSize="md">Data</Th>
+                  <Th fontSize="md">Solicitado Por</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {!isAdmission
+                  ? consults?.consultDebits?.map((item) => (
+                      <Tr key={item.id}>
+                        <Td>{item.name}</Td>
+                        <Td>
+                          {new Intl.NumberFormat("pt-BR", {
+                            currency: "BRL",
+                            style: "currency",
+                          }).format(item.price)}
+                        </Td>
+                        <Td>
+                          {new Intl.DateTimeFormat("pt-BR", {
+                            day: "2-digit",
+                            month: "2-digit",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          }).format(new Date(item?.requestedDate))}
+                        </Td>
+                        <Td>{item.RequestedByVetName}</Td>
+                      </Tr>
+                    ))
+                  : consults?.consultDebits?.map((item) => (
+                      <Tr key={item.id}>
+                        <Td>{item.name}</Td>
+                        <Td>
+                          {new Intl.NumberFormat("pt-BR", {
+                            currency: "BRL",
+                            style: "currency",
+                          }).format(item.price)}
+                        </Td>
+                        <Td>
+                          {new Intl.DateTimeFormat("pt-BR", {
+                            day: "2-digit",
+                            month: "2-digit",
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          }).format(new Date(item?.requestedDate))}
+                        </Td>
+                        <Td>{item.RequestedByVetName}</Td>
+                      </Tr>
+                    ))}
+              </Tbody>
             </Table>
-          
           </TableContainer>
-          <Text m="2" fontWeight="bold">Total: {new Intl.NumberFormat('pt-BR', {currency: 'BRL', style: 'currency'}).format(Number(totalDebits.total))}</Text>
-          <Button  onClick={isAdmission ?  handleCloseAdmission : handleCloseQuery } w='100%' colorScheme="whatsapp">{!isAdmission ? "Finalizar Consulta" : "Encerrar internação"}</Button>
-          </React.Fragment>
-        ))
-       }
-   </Flex>
-  )
+          <Text m="2" fontWeight="bold">
+            Total:{" "}
+            {new Intl.NumberFormat("pt-BR", {
+              currency: "BRL",
+              style: "currency",
+            }).format(
+              Number(
+                quantityMaxDiscont > 0
+                  ? totalDebits.totalWithDiscont
+                  : totalDebits.total
+              )
+            )}
+          </Text>
+          <Button
+            onClick={isAdmission ? handleCloseAdmission : handleCloseQuery}
+            w="100%"
+            colorScheme="whatsapp"
+          >
+            {!isAdmission ? "Finalizar Consulta" : "Encerrar internação"}
+          </Button>
+        </React.Fragment>
+      ))}
+    </Flex>
+  );
 }
